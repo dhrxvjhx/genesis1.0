b645965ff7e749c561684fe1077b70c085af8859
Dhruv Jha <omdhruvjha@gmail.com>
2024-01-30 13:35:06 +0530

    Updated ChatApp.sol

--- contracts/ChatApp.sol
+++ contracts/ChatApp.sol
@@ -1,6 +1,8 @@
+// SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
 contract ChatApp {
+
     struct user {
         string name;
         friend[] friendList;
@@ -12,6 +14,33 @@ contract ChatApp {
     }
 
     struct message {
-        address
+        address sender;
+        uint256 timestamp;
+        string msg;
     }
+
+    mapping(address => user) userList;
+    mapping(bytes32 => message[]) allMessages;
+
+    //CHECK USER EXIST
+    function checkUserExists(address pubkey) public view returns(bool){
+        return bytes(userList[pubkey].name).length > 0;
+    }
+
+    //CREATE ACCOUNT
+    function createAccount(string calldata name) external{
+        require(checkUserExists(msg.sender)==false, "User already exists");
+        require(bytes(name).length>0, "Username cannot be empty");
+
+        userList[msg.sender].name = name;
+    }
+
+    //GET USERNAME
+    function getUsername(address pubkey) external view returns(string memory) {
+        require(checkUserExists(pubkey), "User is not registered");
+        return userList[pubkey].name;
+    }
+
+    //ADD FRIENDS
+    function addFriend(address friend_key, string calldata name)
 }
